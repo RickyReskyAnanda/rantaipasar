c7049b74033191bd677d3cbc170276ae8b24bd58
ricky <rra.rickyresky@gmail.com>
2017-05-20 00:01:33 +0800

    vcx

--- application/modules/P_notifikasi/models/M_notifikasi.php
+++ application/modules/P_notifikasi/models/M_notifikasi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_logged_in extends CI_Model {
+class M_produk extends CI_Model {
 
     public function __construct(){
         parent::__construct();
